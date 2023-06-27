cd346a3b9dddc1184601c1b2ab53c31b28d097a7
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2023-06-27 06:00:13 -0700

    Adds initial package files before R functions

---
+++ R/sablefish-package.R
@@ -0,0 +1,13 @@
+#' sablefish
+#'
+#' The assessment of the status of sablefish off of the U.S. West Coast hinges
+#' on information provided from the results of analyses conducted within this
+#' package. Functions are available to source the data, summarize the data, and
+#' fit the data to Stock Synthesis models.
+#'
+#' @docType package
+#' @name sablefish
+#' @import assertthat
+#' @importFrom utils head tail
+#' @useDynLib sablefish
+NULL
