fd128d6e75aab6d26157468af8a4c22c45300021
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2023-07-25 09:57:08 -0700

    feat(write_named_csvs): Adds fxn to write multiple csv files

--- R/utils.R
+++ R/utils.R
@@ -8,3 +8,34 @@ bind_compositions <- function(x) {
   colnames(one_data_frame) <- no_dots
   return(one_data_frame)
 }
+
+#' Write csv files with same name as object to `dir`
+#'
+#' Pass an unlimited amount of data frames to the function via `...`, i.e.,
+#' `ldots`, and save each data frame as a csv file without row names using the
+#' object name as the file name. The default is to save them in your current
+#' working directory but you can change it to any directory you want. The name
+#' of the function is plural to emphasize that it is vectorized and can
+#' simultaneously save multiple objects to individual files, thanks to
+#' [purrr::map()] and friends.
+#'
+#' @param dir A string providing the file path to the directory where you want
+#'   to save all the csv files. The directory does not need to currently exist
+#'   because it will be created.
+#' @param ... As many data frames as you want. Make sure the objects have good
+#'   names because the names will be used to save the files.
+#' @noRd
+write_named_csvs <- function(..., dir = getwd()) {
+  fs::dir_create(dir)
+  dots <- match.call(expand.dots = FALSE)[["..."]]
+  file_names <- fs::path(
+    dir,
+    paste0(purrr::map_chr(as.list(dots), deparse), ".csv")
+  )
+  purrr::map2(
+    .x = list(...),
+    .y = file_names,
+    .f = \(x, y) utils::write.csv(x = x, file = y, row.names = FALSE)
+  )
+  return(file_names)
+}
