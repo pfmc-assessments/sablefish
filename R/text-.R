9b54a1166033386c7a00aae03105c91222903d8d
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2023-08-04 08:25:54 -0700

    feat(table_likelihoods): Table of likelihoods
    Thanks to code stolen from @iantaylor-NOAA in the [petrale repo](
      https://github.com/pfmc-assessments/lingcod/blob/e3376e490717aaf9cd9e086bedf8442c85548f78/R/table_likelihoods.R
    ).

---
+++ R/text-.R
@@ -0,0 +1,27 @@
+text_likelihoods_used <- function(model) {
+  components_string <- model[["likelihoods_used"]] |>
+    tibble::rownames_to_column() |>
+    dplyr::filter(values != 0, rowname != "TOTAL") |>
+    dplyr::pull(rowname) |>
+    tolower() |>
+    strsplit(split = "_") |>
+    purrr::map_chr(.f = \(x) glue::glue_collapse(x = x, sep = " ")) |>
+    edit_string() |>
+    glue::glue_collapse(sep = ", ", last = ", and ")
+  return(components_string)
+}
+
+edit_string <- function(x,
+                        survey = "indices") {
+  x |>
+  tolower() |>
+  gsub(pattern = "\\bcomp\\b", replacement = "composition") |>
+  gsub(pattern = "\\bdevs\\b", replacement = "deviations") |>
+  gsub(pattern = "\\beq\\b", replacement = "equilibrium") |>
+  gsub(pattern = "\\bequil\\b", replacement = "equilibrium") |>
+  gsub(pattern = "\\biniteq\\b", replacement = "initial equilibrium") |>
+  gsub(pattern = "\\bparm\\b", replacement = "parameter") |>
+  gsub(pattern = "\\bpen\\b", replacement = "penalty") |>
+  gsub(pattern = "\\bsurvey\\b", replacement = survey) |>
+  gsub(pattern = "\\bwt\\b", replacement = "weight")
+}
